0e616dfa0b4e52452a1b8f2b31919d40ab1dae74
Alexander Moore <alexandermoore@users.noreply.github.com>
2023-03-19 13:42:25 -0400

    pre-deployment commit

--- misc/deploy_prod_site.sh
+++ misc/deploy_prod_site.sh
@@ -2,10 +2,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 bash $SCRIPT_DIR/build_prod_site.sh &&
 bash $SCRIPT_DIR/git_add_all.sh
 git commit -am "pre-deployment commit" &&
-git merge master &&
+git checkout master &&
+git pull &&
+git checkout dev &&
+git merge master --no-edit &&
 git commit -am "merge master" &&
 git checkout master &&
-git merge dev &&
+git merge dev --no-edit &&
 git commit -am "merge dev" &&
 git push origin master &&
 git checkout dev
